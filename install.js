f9f9f0ba527b3cbb6258c5340364555cdbbdda46
Jason Morganson <jason@morganson.me>
2018-02-19 19:30:45 +0000

    fix(install): Fix package name

--- install.js
+++ install.js
@@ -5,6 +5,7 @@ const write = require('write-pkg')
 const log = console.log
 const cwd = process.env.INIT_CWD || process.cwd()
 const pkg = path.join(cwd, 'package.json')
+const name = '@jasonmorganson/run-scripts'
 
 const scripts = {
     test: 'run test'
@@ -15,11 +16,11 @@ log('Installing run-scripts')
 read(pkg).then(json => {
 
     // Write runfile
-    const runfile = `module.exports = {\n    ...require('${json.name}')\n}`
+    const runfile = `module.exports = {\n    ...require('${name}')\n}`
     fs.writeFileSync(path.join(cwd, 'runfile.js'), runfile)
 
     // Write tsconfig
-    const tsconfig = `{\n    "extends": "./node_modules/${json.name}/tsconfig.json"\n}`
+    const tsconfig = `{\n    "extends": "./node_modules/${name}/tsconfig.json"\n}`
     fs.writeFileSync(path.join(cwd, 'tsconfig.json'), tsconfig)
 
     // Write package.json scripts
