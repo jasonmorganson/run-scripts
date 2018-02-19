b04becd6c300fd0b6c1ba01a3ae49ce95f263f8d
Jason Morganson <jason@morganson.me>
2018-02-19 16:56:17 +0000

    feat(log): Add some logging to show install progress

--- install.js
+++ install.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const read = require('read-pkg')
 const write = require('write-pkg')
+const log = console.log
 const cwd = process.env.INIT_CWD || process.cwd()
 const pkg = path.join(cwd, 'package.json')
 
@@ -8,7 +9,12 @@ const scripts = {
     test: 'run test'
 }
 
+log('Installing run-scripts')
+
 read(pkg).then(json => {
     json.scripts = scripts
-    write(pkg, json)
+    log('Scripts: ' + Object.keys(scripts).join(', '))
+    write(pkg, json).then(() => {
+        log('Installed run-scripts!')
+    })
 })
