4315e63fef56fae47a139809014ff088de52826c
Michael Singh <Michael.Singh@credsimple.com>
2018-03-27 10:30:10 -0400

    refactor(index): get rid of un-necessary objects

--- rules/index.js
+++ rules/index.js
@@ -3,34 +3,29 @@ const consistentCodeStyle = require('./tslint-consistent-codestyle');
 const immutable = require('./tslint-immutable');
 const microsoft = require('./tslint-microsoft-contrib');
 
-const rules = Object.assign(
-    {
-        "semicolon": [true, "never"],
-        "quotemark": [true, "single"]
-    },
-    immutable,
-    sonarts,
-    consistentCodeStyle,
-    microsoft
-);
-
-const extensions = {
-    "extends": [
-        "tslint:recommended",
-        "tslint-immutable",
-        "tslint-sonarts", 
-        "tslint-consistent-codestyle",
-        "tslint-microsoft-contrib"
-    ]
-};
-
 const tsLintJson = Object.assign(
     {},
-    extensions,
     {
-        rules
+        "extends": [
+            "tslint:recommended",
+            "tslint-immutable",
+            "tslint-sonarts", 
+            "tslint-consistent-codestyle",
+            "tslint-microsoft-contrib"
+        ]
+    },
+    {
+        "rules": Object.assign(
+            {
+                "semicolon": [true, "never"],
+                "quotemark": [true, "single"]
+            },
+            immutable,
+            sonarts,
+            consistentCodeStyle,
+            microsoft
+        )
     }
 );
-
 
 module.exports = tsLintJson;
